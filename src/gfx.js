d70e67e4af6f63e28ae605296eb8f3e22f67d5f7
space8ar <Dmch.d89@gmail.com>
2021-12-14 14:04:31 +0200

    Sphere generation updated

--- src/gfx.js
+++ src/gfx.js
@@ -1,17 +1,27 @@
-function gfxSphereMesh(r, tracks, sectors){
+export function gfxSphereMesh(r, stacks, sectors){
 	const PI = 3.14159265359; 
-	let x = 0,
-		  y = 0,
-		  z = 0;
+	let x 				= 0,
+		  y 				= 0,
+			z 				= 0,
+			xangle 		= 0,
+			yangle 		= 0;
 	let mesh = new Object();
 	mesh.vert   = [];
 	mesh.index  = [];
 	mesh.uv     = [];
 	mesh.normal = []; 
 
-	if (!tracks) || (tracks < 1) || (tracks > 120) || (isNaN(tracks))
-		tracks = 8;  
-	if (!sectors) || (sectors < 1) || (sectors > 120) || (isNaN(sectors))
+	if ((!stacks) || (stacks < 1) || (stacks > 120) || (isNaN(stacks)))
+		stacks = 8;  
+	if ((!sectors) || (sectors < 1) || (sectors > 120) || (isNaN(sectors)))
 		sectors = 8; 
-		
+	for (let i = 0; i < (stacks - 1); i++){
+		let alpha = (180/stacks)*(i);
+		for(let j = 0; j < sectors; j++){
+				let beta = (180/stacks)*(i); 
+				mesh.vert.push(r*Math.cos(alpha)*Math.cos(beta));	// Vertex X coord
+				mesh.vert.push(r*Math.cos(alpha)*Math.sin(beta));	// Vertex Y coord
+				mesh.vert.push(r*Math.sin(alpha));							 	// Vertex Z coord
+		}
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "/src/styles.css";
+import * as gfx from "/src/gfx.js";
 
 var canvas = document.querySelector("#gl_render");
 var gl = canvas.getContext("webgl");
 if (!gl) {
   canvas.style.display = "none";
   document.querySelector("#gl_error").style.display = "block";
+} else {
+  gfx.gfxSphereMesh(1,8,8);
 }
