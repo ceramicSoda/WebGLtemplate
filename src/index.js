d5b3a3de9a9f1b4b65584f5da2196d2a00adae3b
space8ar <Dmch.d89@gmail.com>
2021-12-14 17:35:23 +0200

    Error message update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import * as gfx from "/src/gfx.js";
 var canvas = document.querySelector("#gl_render");
 var gl = canvas.getContext("webgl");
 if (!gl) {
-  canvas.style.display = "none";
-  document.querySelector("#gl_error").style.display = "block";
+  console.error('ERROR: Browser does not support WebGL')
 } else {
-  let Mesh = gfx.gfxSphereMesh(1,16,32);
+  gl.clearColor(0.12, 0.1, 0.12, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  let Mesh = gfx.gfxSphereMesh(1,4,8);
   console.log(Mesh);
 }
