6dd3e5e9378344812332e2df14632f0c9fc22263
space8ar <Dmch.d89@gmail.com>
2021-12-16 12:41:53 +0200

    Minor improvements

--- src/gfx.js
+++ src/gfx.js
@@ -1,13 +1,8 @@
 export function gfxSphereMesh(r, stacks, sectors){
 	const PI = 3.1415; 																//becasuse I hate JS math lib
-	let x 				= 0,
-		  y 				= 0,
-			z 				= 0,
-			xangle 		= 0,
-			yangle 		= 0;
 	let mesh = new Object();
 	mesh.vert   = [];
-	mesh.index  = [];
+	mesh.faces  = [];
 	mesh.uv     = [];
 	mesh.normal = []; 
 
@@ -16,9 +11,9 @@ export function gfxSphereMesh(r, stacks, sectors){
 	if ((!sectors) || (sectors < 1) || (sectors > 120) || (isNaN(sectors)))
 		sectors = 8; 
 
-		mesh.vert.push(0);																		//first vertex X coord
-		mesh.vert.push(0);																		//first vertex Y coord
-		mesh.vert.push(-r);																		//first vertex Z coord
+	mesh.vert.push(0);																		//first vertex X coord
+	mesh.vert.push(0);																		//first vertex Y coord
+	mesh.vert.push(-r);																		//first vertex Z coord
 	for (let i = 1; i < (stacks); i++){
 		let alpha = (180/stacks)*(i)*(PI/180);
 		for(let j = 0; j < sectors; j++){
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var gl = canvas.getContext("webgl");
 if (!gl) {
   console.error('ERROR: Browser does not support WebGL')
 } else {
-  gl.clearColor(0.12, 0.1, 0.12, 1.0);
+  gl.clearColor(0.1, 0.09, 0.11, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   let Mesh = gfx.gfxSphereMesh(1,4,8);
   console.log(Mesh);
