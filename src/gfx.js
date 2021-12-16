0c14fe750b2fc761149ae5419f3becfc89630722
space8ar <Dmch.d89@gmail.com>
2021-12-16 22:56:43 +0200

    IBO prepared

--- src/gfx.js
+++ src/gfx.js
@@ -11,20 +11,29 @@ export function gfxSphereMesh(r, stacks, sectors){
 	if ((!sectors) || (sectors < 1) || (sectors > 120) || (isNaN(sectors)))
 		sectors = 8; 
 
+	//----------------------------calc-verticies------------------
 	mesh.vert.push(0);																		//first vertex X coord
 	mesh.vert.push(0);																		//first vertex Y coord
 	mesh.vert.push(-r);																		//first vertex Z coord
+	let decPoint = Math.pow(10, 3); 
 	for (let i = 1; i < (stacks); i++){
-		let alpha = (180/stacks)*(i)*(PI/180);
-		for(let j = 0; j < sectors; j++){
+		let alpha = (180 / stacks) * (i) * (PI/180);
+		for (let j = 0; j < sectors; j++){
 				let beta = (360/sectors)*(j)*(PI/180); 
-				mesh.vert.push(Math.round(r*Math.cos(alpha)*Math.cos(beta)*1000)/1000);	// Vertex X coord
-				mesh.vert.push(Math.round(r*Math.cos(alpha)*Math.sin(beta)*1000)/1000);	// Vertex Y coord
-				mesh.vert.push(Math.round(r*Math.sin(alpha)								*1000)/1000);	// Vertex Z coord
+				mesh.vert.push(Math.round(r*Math.cos(alpha)*Math.cos(beta)*decPoint)/decPoint);	// Vertex X coord
+				mesh.vert.push(Math.round(r*Math.cos(alpha)*Math.sin(beta)*decPoint)/decPoint);	// Vertex Y coord
+				mesh.vert.push(Math.round(r*Math.sin(alpha)								*decPoint)/decPoint);	// Vertex Z coord
 		}
 	}
 	mesh.vert.push(0);																		//last vertex X coord
 	mesh.vert.push(0);																		//last vertex Y coord
 	mesh.vert.push(r);																		//last vertex Z coord
+	//----------------------------calc-face-indices------------------
+	let indicesCount=(mesh.stacks - 2) * (mesh.sectors) * 2 + mesh.sectors * 2;
+	for (let i = 0; i < indicesCount; i++){
+			
+	}
+
+
 	return(mesh);
 }
