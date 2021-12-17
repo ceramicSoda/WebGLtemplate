da6afb9ea417d50227656ecf9fda3b135616462d
space8ar <Dmch.d89@gmail.com>
2021-12-17 15:17:58 +0200

    ibo update

--- src/gfx.js
+++ src/gfx.js
@@ -1,39 +1,39 @@
-export function gfxSphereMesh(r, stacks, sectors){
-	const PI = 3.1415; 																//becasuse I hate JS math lib
-	let mesh = new Object();
-	mesh.vert   = [];
-	mesh.faces  = [];
-	mesh.uv     = [];
-	mesh.normal = []; 
-
-	if ((!stacks) || (stacks < 1) || (stacks > 120) || (isNaN(stacks)))
-		stacks = 8;  
-	if ((!sectors) || (sectors < 1) || (sectors > 120) || (isNaN(sectors)))
-		sectors = 8; 
-
-	//----------------------------calc-verticies------------------
-	mesh.vert.push(0);																		//first vertex X coord
-	mesh.vert.push(0);																		//first vertex Y coord
-	mesh.vert.push(-r);																		//first vertex Z coord
-	let decPoint = Math.pow(10, 3); 
-	for (let i = 1; i < (stacks); i++){
-		let alpha = (180 / stacks) * (i) * (PI/180);
-		for (let j = 0; j < sectors; j++){
-				let beta = (360/sectors)*(j)*(PI/180); 
-				mesh.vert.push(Math.round(r*Math.cos(alpha)*Math.cos(beta)*decPoint)/decPoint);	// Vertex X coord
-				mesh.vert.push(Math.round(r*Math.cos(alpha)*Math.sin(beta)*decPoint)/decPoint);	// Vertex Y coord
-				mesh.vert.push(Math.round(r*Math.sin(alpha)								*decPoint)/decPoint);	// Vertex Z coord
-		}
-	}
-	mesh.vert.push(0);																		//last vertex X coord
-	mesh.vert.push(0);																		//last vertex Y coord
-	mesh.vert.push(r);																		//last vertex Z coord
-	//----------------------------calc-face-indices------------------
-	let indicesCount=(mesh.stacks - 2) * (mesh.sectors) * 2 + mesh.sectors * 2;
-	for (let i = 0; i < indicesCount; i++){
-			
-	}
-
-
-	return(mesh);
+export function gfxSphereMesh(r, stacks, sectors) {
+  const PI = 3.1415; //becasuse I hate JS math lib
+  let mesh = new Object();
+  mesh.vert   = [];
+  mesh.index  = [];
+  mesh.uv     = [];
+  mesh.normal = [];
+  if (!stacks || stacks < 1 || stacks > 120 || isNaN(stacks)) stacks = 8;
+  if (!sectors || sectors < 1 || sectors > 120 || isNaN(sectors)) sectors = 8;
+  //----------------------------calc-verticies------------------
+  mesh.vert.push(0); //first vertex X coord
+  mesh.vert.push(0); //first vertex Y coord
+  mesh.vert.push(-r); //first vertex Z coord
+  let decPoint = Math.pow(10, 3);
+  for (let i = 1; i < stacks; i++) {
+    let alpha = (180 / stacks) * i * (PI / 180);
+    for (let j = 0; j < sectors; j++) {
+      let beta = (360 / sectors) * j * (PI / 180);
+      mesh.vert.push(
+        Math.round(r * Math.cos(alpha) * Math.cos(beta) * decPoint) / decPoint
+      ); // Vertex X coord
+      mesh.vert.push(
+        Math.round(r * Math.cos(alpha) * Math.sin(beta) * decPoint) / decPoint
+      ); // Vertex Y coord
+      mesh.vert.push(Math.round(r * Math.sin(alpha) * decPoint) / decPoint); // Vertex Z coord
+    }
+  }
+  mesh.vert.push(0); //last vertex X coord
+  mesh.vert.push(0); //last vertex Y coord
+  mesh.vert.push(r); //last vertex Z coord
+  //----------------------------calc-face-indices------------------
+  let indicesCount = (stacks - 2) * sectors * 2 + sectors * 2;
+  for (let i = 0; i < indicesCount; i++) {
+    if      (i < (sectors-1)) {mesh.index[i]=(0, i+1, i+2);}
+    else if (i == (sectors-1)) {mesh.index[i]=(0, i+1, 1);}
+  }
+  console.log(mesh);
+  return mesh;
 }
--- src/index.js
+++ src/index.js
@@ -9,5 +9,4 @@ if (!gl) {
   gl.clearColor(0.1, 0.09, 0.11, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   let Mesh = gfx.gfxSphereMesh(1,4,8);
-  console.log(Mesh);
 }
