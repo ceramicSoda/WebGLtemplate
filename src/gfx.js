5e2b3afc6218259d59a2be3b0ade37e2f90e80bf
space8ar <Dmch.d89@gmail.com>
2021-12-18 18:47:02 +0200

    Material added

--- src/gfx.js
+++ src/gfx.js
@@ -1,3 +1,22 @@
+//----------------------------   DEFAULT SHADERS  -----------------------//
+const defaultVert = `
+attribute vec4 aVertexPosition;
+attribute vec4 aVertexColor;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+varying lowp vec4 vColor;
+void main(void) {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  vColor = aVertexColor;
+}
+`;
+const defaultFrag = `
+varying lowp vec4 vColor;
+void main(void) {
+  gl_FragColor = vColor;
+}
+`;
+//----------------------------   SPHERE MESH  -----------------------//
 export function gfxSphereMesh(r, stacks, sectors) {
   if (!stacks || stacks < 1 || stacks > 120 || isNaN(stacks)) stacks = 8;
   if (!sectors || sectors < 1 || sectors > 120 || isNaN(sectors)) sectors = 8;
@@ -8,12 +27,11 @@ export function gfxSphereMesh(r, stacks, sectors) {
   mesh.index  = [];
   mesh.uv     = [];
   mesh.normal = [];
-  const PI = 3.1415; //becasuse I hate JS math lib
-
-  //----------------------------calc-verticies------------------
-  mesh.vert.push(0); //first vertex X coord
-  mesh.vert.push(0); //first vertex Y coord
-  mesh.vert.push(r); //first vertex Z coord
+  const PI = 3.1415;
+  //---calc-verticies
+  mesh.vert.push(0);
+  mesh.vert.push(0);
+  mesh.vert.push(r);
   let decPoint = Math.pow(10, 3);
   for (let i = 1; i < stacks; i++) {  
     let alpha = (180 / stacks) * i * (PI / 180);
@@ -22,16 +40,15 @@ export function gfxSphereMesh(r, stacks, sectors) {
       let x = r * Math.sin(alpha) * Math.cos(beta);
       let y = r * Math.sin(alpha) * Math.sin(beta);
       let z = r * Math.cos(alpha); 
-      mesh.vert.push(Math.round(x * decPoint) / decPoint); // Vertex X coord
-      mesh.vert.push(Math.round(y * decPoint) / decPoint); // Vertex Y coord
-      mesh.vert.push(Math.round(z * decPoint) / decPoint); // Vertex Z coord
+      mesh.vert.push(Math.round(x * decPoint) / decPoint);
+      mesh.vert.push(Math.round(y * decPoint) / decPoint);
+      mesh.vert.push(Math.round(z * decPoint) / decPoint);
     }
   }
-  mesh.vert.push(0); //last vertex X coord
-  mesh.vert.push(0); //last vertex Y coord
-  mesh.vert.push(-r); //last vertex Z coord
-
-  //----------------------------calc-face-indices------------------
+  mesh.vert.push(0);
+  mesh.vert.push(0);
+  mesh.vert.push(-r);
+  //---calc-face-indices
   for (let i = 0; i < stacks; i++){
     for (let j = 0; j < sectors; j++){
       if (i === 0){ 
@@ -69,6 +86,13 @@ export function gfxSphereMesh(r, stacks, sectors) {
       }
     }
   }
-  console.log(mesh);
   return mesh;
+}
+//----------------------------   MATERIAL  -----------------------//
+class GfxMaterial{
+  constructor(vert, frag){
+    this.vert = (!vert) ? vert : defaultVert; 
+    this.vert = (!frag) ? frag : defaultFrag; 
+    
+  }
 }
